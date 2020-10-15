d70078c3617924417f5d2d433e90b6ccfe3424f1
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-10-15 22:59:09 +0900

    Application > Shared Mutable State

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Mutex;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
@@ -18,21 +20,28 @@ struct AppState {
     app_name: String,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // Mutex is necessary to mutate safely across threads
+}
 
-    format!("Hello, {}", app_name)
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {}", counter)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .data(AppState {
-                app_name: String::from("Actix-web"),
-            })
-            .service(index)
+            // Note: using app_data instead of data
+            .app_data(counter.clone()) // <- register the created data
+            .route("/", web::get().to(index))
     })
     .bind("127.0.0.1:8080")?
     .run()
