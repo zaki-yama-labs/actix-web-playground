87e27bb1c2d6b429a372a5d177fe392c1a77f552
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-10-15 19:29:27 +0900

    Application > State

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,25 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-async fn index() -> impl Responder {
-    "Hello world!"
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+
+    format!("Hello, {}", app_name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            // prefixed all resources and routes attached to it...
-            web::scope("/app")
-                // ...so this handles request for `GET /app/index.html`
-                .route("/index.html", web::get().to(index)),
-        )
-        // .service(hello)
-        // .service(echo)
-        // .route("/hey", web::get().to(manual_hello))
+        App::new()
+            .data(AppState {
+                app_name: String::from("Actix-web"),
+            })
+            .service(index)
     })
     .bind("127.0.0.1:8080")?
     .run()
