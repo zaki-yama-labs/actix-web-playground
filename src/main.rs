c15cb7e0a675f51da039bb9da95964454f2c6e89
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-10-15 19:28:48 +0900

    Application > scope

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -15,13 +14,22 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-        .service(hello)
-        .service(echo)
-        .route("/hey", web::get().to(manual_hello))
+        App::new().service(
+            // prefixed all resources and routes attached to it...
+            web::scope("/app")
+                // ...so this handles request for `GET /app/index.html`
+                .route("/index.html", web::get().to(index)),
+        )
+        // .service(hello)
+        // .service(echo)
+        // .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
     .run()
